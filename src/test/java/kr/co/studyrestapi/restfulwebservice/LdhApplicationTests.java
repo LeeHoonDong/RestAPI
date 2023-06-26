d0030460039c8a0cc72e70c96bb8dcc9ff016856
LeeHoonDong <ldhoon0813@gmail.com>
2023-06-26 20:42:00 +0900

    first commit

---
+++ src/test/java/kr/co/studyrestapi/restfulwebservice/LdhApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.studyrestapi.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
