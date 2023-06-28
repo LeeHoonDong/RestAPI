f9f3db03fceabaffc5844163a2ae61ab6ac23f45
LeeHoonDong <ldhoon0813@gmail.com>
2023-06-29 00:49:18 +0900

    Add. comment of ExceptionHandling Part2.

--- src/main/java/kr/co/studyrestapi/restfulwebservice/Exception/UserNotFoundException.java
+++ src/main/java/kr/co/studyrestapi/restfulwebservice/Exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package kr.co.studyrestapi.restfulwebservice.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND) //클라이언트에게 500번대 에러가 아닌 400번대 에러를 반환.
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
